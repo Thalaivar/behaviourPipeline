a8544fbb78e7114026efa0fbf56c3065fafda3a8
Thalaivar <dhruvlaad@gmail.com>
2021-08-25 21:06:45 +0530

    cluster filter error fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='behaviourPipeline',
-    version='0.1.1',
+    version='0.1.2',
     author='Dhruv Laad',
     author_email='dhruvlaad@gmail.com',
     packages=['behaviourPipeline'],
