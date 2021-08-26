5622bf693b8819d65829fc4303da4dc0cee7c9f7
Thalaivar <dhruvlaad@gmail.com>
2021-08-26 20:47:00 +0530

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='behaviourPipeline',
-    version='0.1.1',
+    version='0.2.0',
     author='Dhruv Laad',
     author_email='dhruvlaad@gmail.com',
     packages=['behaviourPipeline'],
