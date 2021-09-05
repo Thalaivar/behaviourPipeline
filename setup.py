fe8c63103e5adc22070cceddb7e2b0c6c7dde47a
Thalaivar <dhruvlaad@gmail.com>
2021-09-05 14:07:40 +0530

    outdir argument fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='behaviourPipeline',
-    version='0.2.7',
+    version='0.2.9',
     author='Dhruv Laad',
     author_email='dhruvlaad@gmail.com',
     packages=['behaviourPipeline'],
